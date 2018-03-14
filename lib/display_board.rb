c88a2294f520068c269310244d826f5ad184ecd9
dzeit216@gmail.com Zeit <dzeit216@gmail.com>
2018-03-14 21:57:17 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,18 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board 
+<<<<<<< HEAD
   puts "   |   |   " 
   puts "-----------" 
   puts "   |   |   "
   puts "-----------" 
   puts "   |   |   "
+=======
+  puts "A Tic Tac Toe Board"
+  puts "   |   |  " 
+  puts "-----------" 
+  puts "   |   |  "
+  puts "-----------" 
+  puts "   |   |  "
+>>>>>>> d66432c0f91de83970dbbb67dd06772d56b0287d
 end
 
